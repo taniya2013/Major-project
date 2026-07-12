01fed7c18a4e07ef835996853995bbc7d8b31b24
Taniya <tt835554@gmail.com>
2026-07-12 15:53:12 +0530

    Delete AI_Transcription_System directory

--- AI_Transcription_System/check_model.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AQ.Ab8RN6IqdO5rdk__cPu5EyElZVrV-65TiulmjX1tYIiG5lfcSg")
-
-for model in genai.list_models():
-    if "generateContent" in model.supported_generation_methods:
-        print(model.name)
